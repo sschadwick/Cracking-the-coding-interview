02ee38cb4fb91865ecb418967f35926c86ba8442
“sschadwick” <sschadwick@gmail.comssh-keygen -t rsa -C sschadwick@gmail.com>
2016-01-08 10:46:46 -0800

    complete 1.6

--- Ch1_Arrays_and_Strings/1.6.js
+++ Ch1_Arrays_and_Strings/1.6.js
@@ -1,1 +1,29 @@
 // String Compressions: 1.6
+
+function compress(str) {
+  var split = str.split('');
+  var counter = 1;
+  var result = '';
+  for (var i = 0; i < split.length; i++) {
+    if (split[i] === split[i + 1]) {
+      counter++;
+    } else {
+      result += split[i] + counter;
+      counter = 1;
+    }
+  }
+  if (str.length > result.length) {
+    return result;
+  }  
+  return str;
+};
+
+var input1 = 'aaaaabbbbbbcccd';
+var output1 = 'a5b6c3d1';
+// true
+console.log((compress(input1) === output1));
+
+var input2 = 'abcdef';
+var output2 = 'abcdef';
+// true
+console.log((compress(input2) === output2));
